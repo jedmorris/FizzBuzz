66f3bf1ffc8f89e00bcc8086295ac716b7c984d3
Jed Morris <jemorris110@gmail.com>
2022-01-18 13:41:07 -0800

    added getValues & generateNumbers to site.js

--- js/site.js
+++ js/site.js
@@ -8,3 +8,49 @@ function getMessage() {
 	)
 }
 
+function getValues() {
+	let firstNumber = document.getElementById("firstNumber").value;
+	let secondNumber = document.getElementById("secondNumber").value;
+
+	firstNumber = parseInt(firstNumber);
+	secondNumber = parseInt(secondNumber);
+
+	if (Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
+
+		generateNumbers(firstNumber, secondNumber);
+	} else {
+		Swal.fire({
+			icon: 'error',
+			title: 'Whoops...',
+			text: 'Only Integers are allowed for FizzBuzz.'
+		})
+	}
+}
+
+function generateNumbers(firstNumber, secondNumber) {
+	let numbers = [];
+	for (let i = firstNumber; i < secondNumber; i++) {
+		numbers.push(i);
+	}
+	return numbers;
+}
+
+function displayNumbers(numbers) {
+	let templateRows = "";
+	let className = "";
+
+	for (let i = 0; i < numbers.length; i++) {
+
+		let number = numbers[i];
+
+		if (number % 2 == 0) {
+			className = "even"
+		} else {
+			className = "odd"
+		}
+
+		let row = `<tr><td> class="${className}">${number}</td></tr>`;
+		templateRows += row;
+	}
+	document.getElementById("results").innerHTML = templateRows;
+}
